1a3572ff806627ffd242e38af3c5ddd10571d93a
sanskrutithorat <sanskrutithorat01@gmail.com>
2026-07-08 14:50:22 +0530

    feat: initialize project settings with apps, CORS, and REST framework configuration

--- djangoSolutions/settings/settings.py
+++ djangoSolutions/settings/settings.py
@@ -76,6 +76,9 @@ ROOT_URLCONF = 'settings.urls'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
